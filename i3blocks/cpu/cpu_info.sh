d82a69b9fb796d4362315b946869e2c74521e626
Arjun G Krishna <arjunradhakrishnan52@gmail.com>
2020-11-11 00:06:51 +0530

    back up i3 dotfiles

---
+++ i3blocks/cpu/cpu_info.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+TEMP=$(sensors | grep 'edge' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
+CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
+echo "$CPU_USAGE $TEMP" | awk '{ printf(" 🛠️ CPU:%6s% @ %s \n"), $1, $2 }'
